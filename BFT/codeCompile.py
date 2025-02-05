1dc32347e402f2f24f17616ebb0af0700841586a
Florian2307 <Florian.schoch@buhlergroup.com>
2025-02-05 16:37:17 +0100

    The last version was not finishes this is almost

--- BFT/codeCompile.py
+++ BFT/codeCompile.py
@@ -1,5 +1,4 @@
 import numpy as np
-import yolo11
 import timeit
 import os 
 
@@ -9,10 +8,10 @@ codes_path = os.path.join(script_dir, 'codes.csv')
 
 # CSV-Datei als NumPy-Array laden
 tensor = np.array([
-    [50,  30, 100, 80, 0.9, 0],  # "loop"
-    [110, 35, 130, 55, 0.95, 14],  # 4
-    [50, 100, 100, 150, 0.8, 7],  # "lampe1"
-    [50, 170, 100, 220, 0.85, 10]  # "Pausiere"
+   [50,  30, 100, 80, 0.9, 2],    # "if"
+   [110, 35, 130, 55, 0.95, 4],   # "tasterGedrückt" 
+   [150, 35, 170, 55, 0.9, 6],    # "lampeEin"
+   [50, 100, 100, 150, 0.8, 3],   # "endIf"
 ])
 # Konfidenzschwelle
 confidence_threshold = 0.5
@@ -36,8 +35,7 @@ for row in sorted_tensor:
     blocks.append(block)
  
 codes = (
-    "loop", "endLoop", "if", "endIf", "taster1Gedrückt", "taster2Gedrückt",
-    "schalter1Umgelegt", "lampe1", "lampe2", "lampe3", "Pausiere", "1", "2", "3",
+    "loop", "endLoop", "if", "endIf", "tasterGedrückt", "tasterNichtGedrückt", "lampeEin","lampeAus","Pausiere", "1", "2", "3",
     "4", "5", "6", "7", "8", "9", "10", "unendlich"
 )
   
@@ -51,7 +49,7 @@ def get_indentation(level):
 
 def find_count(block, blocks, tolerance=50, toleranceY=-50):
     for candidate in blocks:
-        if 11 <= candidate["class_id"] <= 21:
+        if 9 <= candidate["class_id"] <= 19:
             y_diff = candidate["y_min"] - block["y_min"]
             if (abs(candidate["x_min"] - block["x_max"]) <= tolerance and
                 (y_diff <= tolerance and y_diff >= toleranceY)):
@@ -59,6 +57,23 @@ def find_count(block, blocks, tolerance=50, toleranceY=-50):
                 return "unendlich" if candidate["class_id"] == 21 else str(candidate["class_id"] - 10)
                 
     return "1"
+
+
+def findTaster(block, blocks, tolerance=50, toleranceY=-50):
+    for candidate in blocks:
+        if candidate["class_id"] in [4, 5]:
+            y_diff = candidate["y_min"] - block["y_min"]
+            if (abs(candidate["x_min"] - block["x_max"]) <= tolerance and
+                (y_diff <= tolerance and y_diff >= toleranceY)):
+                
+                if candidate["class_id"] == 4:
+                    return "taster"
+                
+                if candidate["class_id"] == 5:
+                    return "not taster"
+                
+    return "True"
+
 
  
 for block in blocks:
@@ -75,14 +90,22 @@ for block in blocks:
         elif code == "endLoop":
             if indentation_level > 0:
                 indentation_level -= 1  # Einrückung reduzieren
-            output_lines.append(f"{get_indentation(indentation_level)}\n")  # Leerzeile
-        elif code == "lampe1":
+        elif code == 'if':
+            bedinnung = findTaster(block, blocks)
+            output_lines.append(f"{get_indentation(indentation_level)}if({bedinnung}):\n")
+            indentation_level += 1
+        elif code == 'endIf':
+            if indentation_level > 0:
+                indentation_level -= 1
+        elif code == "lampeEin":
             output_lines.append(f"{get_indentation(indentation_level)}lampe.turn_on()\n")
+        elif code == "lampeAus":
+            output_lines.append(f"{get_indentation(indentation_level)}lampe.turn_off()\n")
         elif code == "Pausiere":
             paus_count = find_count(block, blocks)
             if paus_count == "unendlich":
                 paus_count = 1
-            output_lines.append(f"{get_indentation(indentation_level)}time.sleep(1)\n")
+            output_lines.append(f"{get_indentation(indentation_level)}time.sleep({paus_count})\n")
     else:
         print(f'invalid class id ${class_id}')
 
