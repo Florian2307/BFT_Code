ce427c79bb27f6c68a5893b112a2088a8d3bab76
Tobias080108 <tobias.kuhn@buhlergroup.com>
2025-06-02 11:59:11 +0200

    Update Tensor.csv with new data, modify image path in codeCompile.py, adjust LED blink count in final.py, and add multiple image files for collection

--- BFT/final.py
+++ BFT/final.py
@@ -4,7 +4,7 @@ from time import sleep
 led = LED(17)
 button = Button(3)
 
-for i in range(4):
-    led.on()
-    sleep(1)
+for i in range(1):
     led.off()
+    if button.value == 1:
+        led.off()
